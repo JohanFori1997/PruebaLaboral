565263356c0e907880ddf304d05c572b21ca6344
JohanFori1997 <87413649+JohanFori1997@users.noreply.github.com>
2021-07-14 02:25:20 -0500

    Backend Prueba Laboral
    Aquí el desarrollo del Backend

---
+++ CRUDLab/CRUDLab/Models/Request/PersonaRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDLab.Models.Request
+{
+    public class PersonaRequest
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public int edad { get; set; }
+        public string email { get; set; }
+    }
+}
